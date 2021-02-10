845b999f97075b40e851c446b5242a24774f9317
Dmitry_Neustupov <neustupov@yandex.ru>
2021-02-10 19:07:29 +0300

    Добавил тесты контроллера

--- src/main/java/org/neustupov/springboot2bestpractics/todo/domain/ToDo.java
+++ src/main/java/org/neustupov/springboot2bestpractics/todo/domain/ToDo.java
@@ -12,7 +12,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ToDo {
 
-  @NotNull
+  @NotNull(message = "Id is not null")
   String id;
   @NotNull
   @NotBlank
--- src/test/java/org/neustupov/springboot2bestpractics/todo/controller/ToDoControllerTest.java
+++ src/test/java/org/neustupov/springboot2bestpractics/todo/controller/ToDoControllerTest.java
@@ -1,13 +1,14 @@
 package org.neustupov.springboot2bestpractics.todo.controller;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,7 +31,7 @@ class ToDoControllerTest {
   private MockMvc mvc;
 
   @MockBean
-  private CommonRepository repository;
+  private CommonRepository<ToDo> repository;
 
   @Test
   @DisplayName("GET /api/todo")
@@ -42,31 +44,115 @@ class ToDoControllerTest {
     mvc.perform(get("/api/todo")
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        //.andExpect(header().string(HttpHeaders.LOCATION, "/rest/widgets"))
-        .andExpect(jsonPath("$", hasSize(1)));
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].description", is("First ToDo")));
   }
 
   @Test
-  void getToDoById() {
+  @DisplayName("GET /api/todo/{id}")
+  void getToDoById() throws Exception {
+    // Setup our mocked service
+    ToDo todo = ToDoBuilder.create().withId("11-22-33").build();
+    doReturn(Optional.of(todo)).when(repository).findById("11-22-33");
+
+    mvc.perform(get("/api/todo/{id}", "11-22-33")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is("11-22-33")));
   }
 
   @Test
-  void setCompleted() {
+  @DisplayName("PATCH /api/todo/{id}")
+  void setCompleted() throws Exception {
+    // Setup our mocked service
+    ToDo todo = ToDoBuilder.create().withId("11-22-33").build();
+    doReturn(todo).when(repository).findById("11-22-33");
+
+    mvc.perform(patch("/api/todo/{id}", "11-22-33")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost/api/todo/11-22-33"));
+
   }
 
   @Test
-  void createToDo() {
+  @DisplayName("POST /api/todo")
+  void createToDoPost() throws Exception {
+    // Setup our mocked service
+    ToDo todoToPost = ToDoBuilder.create().withId("11-22-33").withDescription("POST Todo").build();
+    cleanDateFields(todoToPost);
+    ToDo todoToReturn = ToDoBuilder.create().withId("11-22-33").build();
+    doReturn(todoToReturn).when(repository).save((ToDo) any());
+
+    mvc.perform(post("/api/todo")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(asJsonString(todoToPost)))
+        .andExpect(status().isCreated())
+        .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost/api/todo/11-22-33"));
   }
 
   @Test
-  void deleteToDo() {
+  @DisplayName("PUT /api/todo")
+  void createToDoPut() throws Exception {
+    // Setup our mocked service
+    ToDo todoToPost = ToDoBuilder.create().withId("11-22-33").withDescription("PUT Todo").build();
+    cleanDateFields(todoToPost);
+    ToDo todoToReturn = ToDoBuilder.create().withId("11-22-33").build();
+    doReturn(todoToReturn).when(repository).save((ToDo) any());
+
+    mvc.perform(put("/api/todo")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(asJsonString(todoToPost)))
+        .andExpect(status().isCreated())
+        .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost/api/todo/11-22-33"));
   }
 
   @Test
-  void deleteToDo1() {
+  @DisplayName("POST /api/todo with not valid data")
+  void createToDoPostNotValidData() throws Exception {
+    // Setup our mocked service
+    ToDo todoToPost = ToDoBuilder.create().withDescription("POST Todo").build();
+    todoToPost.setId(null);
+    cleanDateFields(todoToPost);
+
+    mvc.perform(post("/api/todo")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(asJsonString(todoToPost)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.errors.[0]", is("Id is not null")));
   }
 
   @Test
-  void handleException() {
+  @DisplayName("DELETE /api/todo/{id}")
+  void deleteToDo() throws Exception {
+    mvc.perform(delete("/api/todo/{id}", "11-22-33")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNoContent());
+  }
+
+  @Test
+  @DisplayName("DELETE /api/todo")
+  void deleteToDo1() throws Exception {
+    // Setup our mocked service
+    ToDo todo = ToDoBuilder.create().withId("11-22-33").build();
+    cleanDateFields(todo);
+
+    mvc.perform(delete("/api/todo")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(asJsonString(todo)))
+        .andExpect(status().isNoContent());
+  }
+
+  private static String asJsonString(final Object obj) {
+    try {
+      return new ObjectMapper().writeValueAsString(obj);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void cleanDateFields(ToDo todo){
+    todo.setCreated(null);
+    todo.setModified(null);
   }
 }
